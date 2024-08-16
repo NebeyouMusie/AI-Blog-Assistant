5952841c83f2ec6042580d958f32e73000604e3e
Nebeyou Musie <Nebeyoumusie@gmail.com>
2024-08-16 11:03:57 +0000

    update file

--- app/app.py
+++ app/app.py
@@ -1,39 +1,10 @@
 import re
 import streamlit as st
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from utils import get_llm
+from utils import get_llm, get_prompt_template_for_title_suggestion, get_prompt_template_for_title
 
-llm = get_llm()
+title_suggestion_chain = get_prompt_template_for_title_suggestion()
+title_chain = get_prompt_template_for_title()
 
-# define a ChatPromptTempalte for title suggestions
-prompt_template_for_title_suggestion = ChatPromptTemplate.from_template(
-    """I'm planning a blog post on topic : {topic}.
-    The title is informative, or humorous, or persuasive. 
-    The target audience is : {target}  
-    Suggest a list of ten creative and attention-grabbing titles for this blog post. 
-    Don't give any explanation or overview to each title.
-    IMPORTANT: ONLY RETURN THE TOPICS NOTHING MORE.
-    """
-)
-
-# define the title suggestion chain
-title_suggestion_chain = prompt_template_for_title_suggestion | llm | StrOutputParser()
-
-
-# define a ChatPromptTemplate for blog content generation
-prompt_tempalte_for_title = ChatPromptTemplate.from_template(
-    """Write a high-quality, informative, and plagiarism-free blog post on the topic: "{title}". 
-    Target the content towards a {target} audience. 
-    Use a conversational writing style and structure the content with an introduction, body paragraphs, and a conclusion. 
-    Try to incorporate these keywords: {keywords}. 
-    Aim for a content length of {blog_length} words. 
-    Make the content engaging and capture the reader's attention.
-    """
-)
-
-# define the title generation chain
-title_chain = prompt_tempalte_for_title | llm | StrOutputParser()
 
 # The UI(User Interface)
 st.set_page_config(page_title='AI Blog Assistant', page_icon='📰')
@@ -48,7 +19,6 @@ with topic_expander:
     submit_btn = st.button("Submit")
 
 if submit_btn: # Handle button click (submit_topic)
-    title_selection_text = '' # Initialize an empty string to store title suggestions
     title_suggestion_str = title_suggestion_chain.invoke({'topic': topic_name, 'target': target_audience})
     st.text(title_suggestion_str)
     
@@ -57,8 +27,7 @@ title_expander = st.expander("Input the title") # Create an expander for title i
 
 with title_expander:
     title_of_the_blog = st.text_input("", key="title_of_the_blog")
-    num_of_words = st.slider("Number of words", min_value=100, max_value=1000, step=50)
-    
+    num_of_words = st.number_input("Number of words", min_value=100, max_value=1000, step=50)
     if 'keywords' not in st.session_state:
         st.session_state.keywords = []
     keyword_input = st.text_input("Enter a keyword: ")
@@ -67,4 +36,16 @@ with title_expander:
         st.session_state.keywords.append(keyword_input)
         st.session_state.keyword_input = ""
         for keyword in st.session_state.keywords:
-            st.write(f"<div style='display: inline-block; background-color: lightgray; padding: 5px; margin: 5px;'>{keyword}</div>", unsafe_allow_html=True)
+            st.write(f"<div style='display: inline-block; background-color: green; padding: 10px; margin: 5px; border: none; border-radius: 10px;'>{keyword}</div>", unsafe_allow_html=True)
+            
+    submit_title = st.button("Submit Info")
+    
+if submit_title:
+    formatted_keywords = []
+    for i in st.session_state.keywords:
+        if len(i) > 0:
+            formatted_keywords.append(i.lstrip('0123456789 : ').strip('"').strip("'"))
+    formatted_keywords = ', '.join(formatted_keywords)
+    st.subheader(title_of_the_blog)
+    generated_blog_content = title_chain.invoke({'title': title_of_the_blog, 'target': target_audience, 'keywords': formatted_keywords, 'blog_length': num_of_words})
+    st.write(generated_blog_content)
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,6 @@
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from config import load_config, get_groq_api_key
 
 load_config()
@@ -6,3 +8,40 @@ load_config()
 def get_llm():
     llm  = ChatGroq(api_key=get_groq_api_key(), model='llama-3.1-70b-versatile', temperature=0.7)
     return llm
+
+
+def get_prompt_template_for_title_suggestion():
+    # define a ChatPromptTempalte for title suggestions
+    prompt_template_for_title_suggestion = ChatPromptTemplate.from_template(
+        """I'm planning a blog post on topic : {topic}.
+        The title is informative, or humorous, or persuasive. 
+        The target audience is : {target}  
+        Suggest a list of ten creative and attention-grabbing titles for this blog post. 
+        Don't give any explanation or overview to each title.
+        IMPORTANT: ONLY RETURN THE TOPICS NOTHING MORE.
+        """
+    )
+
+    # define the title suggestion chain
+    title_suggestion_chain = prompt_template_for_title_suggestion | get_llm() | StrOutputParser()
+
+    
+    return title_suggestion_chain
+
+
+def get_prompt_template_for_title():
+    # define a ChatPromptTemplate for blog content generation
+    prompt_tempalte_for_title = ChatPromptTemplate.from_template(
+        """Write a high-quality, informative, and plagiarism-free blog post on the topic: "{title}". 
+        Target the content towards a {target} audience. 
+        Use a conversational writing style and structure the content with an introduction, body paragraphs, and a conclusion. 
+        Try to incorporate these keywords: {keywords}. 
+        Aim for a content length of {blog_length} words. 
+        Make the content engaging and capture the reader's attention.
+        """
+    )
+
+    # define the title generation chain
+    title_chain = prompt_tempalte_for_title | get_llm() | StrOutputParser()
+    
+    return title_chain
