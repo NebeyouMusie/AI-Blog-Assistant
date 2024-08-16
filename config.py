97aa485aa71b767013a13b60f47c70517698aa22
Nebeyou Musie <Nebeyoumusie@gmail.com>
2024-08-16 09:18:06 +0000

    update file

---
+++ config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+def load_config():
+    return load_dotenv()
+
+def get_groq_api_key():
+    return os.getenv('GROQ_API_KEY')
---
+++ utils.py
@@ -0,0 +1,8 @@
+from langchain_groq import ChatGroq
+from config import load_config, get_groq_api_key
+
+load_config()
+
+def get_llm():
+    llm  = ChatGroq(api_key=get_groq_api_key(), model='llama-3.1-70b-versatile')
+    return llm
